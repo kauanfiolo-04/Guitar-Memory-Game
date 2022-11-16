b11a4d9531dfe9101ae142866bb5d99c287f2eda
Kauan Fiolo da Silva <102124569+kauanfiolo-04@users.noreply.github.com>
2022-11-16 16:03:12 -0300

    Add files via upload
    Terminei a pagina de login

---
+++ js/login.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('.login_Input')
+const button = document.querySelector('.login_Button')
+const form=document.querySelector('.login_Form')
+
+const validateInput = ({target}) => {
+    if(target.value.length > 2){
+        button.removeAttribute('disabled')
+        return
+    }
+
+    button.setAttribute('disabled','')  
+}
+
+const handleSubmit=(event)=>{
+    event.preventDefault()
+    
+    localStorage.setItem('player', input.value)
+    window.location='pages/game.html'
+}
+
+input.addEventListener('input',validateInput)
+form.addEventListener('submit',handleSubmit)
